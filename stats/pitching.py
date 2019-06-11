807e5c38ff2ddd8f0d3c9060a958d66abb544ce0
Kumaravelu Sarabojimahalingam (Cognizant) <kumaravelu_sarabojimahalingam@belk.com>
2019-06-11 12:28:42 -0400

    commit

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import games 
+
+plays = games[games["type"] == "play"]
+strike_outs = plays[plays["events"].str.contains('K')]
+strike_outs = strike_outs.groupby(["year", "game_id"]).size()
+
+strike_outs  = strike_outs.reset_index(name="strike_outs")
+
+strike_outs = strike_outs.loc[:, ["year", "strike_outs"]].apply(pd.to_numeric)
+
+strike_outs.plot(kind="scatter", x="year", y="strike_outs").legend("Strike Outs")
+plt.xlabel("Year")
+plt.ylabel("Strike Outs")
+plt.title("scatter plot")
+plt.show()
